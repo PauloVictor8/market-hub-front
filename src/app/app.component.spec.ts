3476fb148598d43e8aa06382677326f0614f39fc
Victor <paulo9.victor.silva@gmail.com>
2024-07-25 18:08:37 -0300

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'giga-ecommerce-front' title`, () => {
+  it(`should have the 'market-hub' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('giga-ecommerce-front');
+    expect(app.title).toEqual('market-hub');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, giga-ecommerce-front');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, market-hub');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', // eslint-disable-line
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'giga-ecommerce-front';
+  title = 'market-hub';
 }
